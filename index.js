8332c8e030b2871ba7cc8fc4a737605a912d4ffa
Samuel Ondrek <h50ibva@s-mxs.net>
2021-01-30 11:06:47 +0100

    Website update from an old one

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express"
+import path from "path"
+import favicon from "serve-favicon"
+import helmet from "helmet"
+import compression from "compression"
+import ejs from "ejs"
+
+const app = express()
+let server = null
+
+app.use("/public", express.static(path.join(path.resolve(), "/website/public")))
+app.use(favicon(path.join(process.cwd(), "/website/public/favicons/favicon.ico")))
+app.use(helmet())
+app.use(compression())
+app.disable("x-powered-by")
+
+app.set("views", path.join(process.cwd(), "/website/views"))
+app.set("view engine", "html")
+app.engine("html", ejs.__express)
+
+app.get("/", (req, res) => {
+  res.setHeader("Content-type", "text/html")
+  res.render("index", { data: "Sample Data" })
+})
+
+server = app.listen(process.env.PORT || 8020)
+console.info("website :: Serving on localhost:8020")
+
+function cleanup() {
+  server.close()
+}
+
+process.on("SIGINT", cleanup)
+process.on("SIGTERM", cleanup)
